73ee08db017a7792bd7f4c66880793cef8ce31e2
Liebb <liebermorazan@gmail.com>
2023-01-18 11:32:44 -0600

    Se acorrigen algunos campos de la BD y se realiza una carga inicial de datos

--- TableScripts/Pets.sql
+++ TableScripts/Pets.sql
@@ -1,14 +1,16 @@
 USE PETS_DB
 GO
 DROP TABLE IF EXISTS 
-    [Person].[User],
-    [Person].Employee,
-    [Person].Person,
-    [Security].Rol,
-    [Security].Connection,
-    [Person].Salary,
+    [Account].[Date],
+    [Account].Request,
+    [Pet].Dogs,
     [Pet].Breeds,
-    [Pet].Dogs
+    [Security].Connection,
+    [Account].[User],
+    [Person].Employee,
+    [Person].Salary,
+    [Person].Person,
+    [Security].Rol
 GO
 CREATE TABLE [Security].Rol(
     IdRol int IDENTITY(1,1) NOT NULL
@@ -21,6 +23,14 @@ CREATE TABLE [Security].Rol(
         CONSTRAINT DF_State_IsActive
         DEFAULT 1,
     RolInfo VARCHAR(50) NULL
+);
+GO
+CREATE TABLE [Person].Salary(
+    IdSalary int IDENTITY(1,1) NOT NULL
+        CONSTRAINT PK_Salary_IdSalary
+            PRIMARY KEY CLUSTERED,
+    Quantity money NOT NULL,
+    [Date] datetime default GETDATE()
 );
 GO
 CREATE TABLE [Person].Person(
@@ -55,6 +65,9 @@ CREATE TABLE [Person].Employee(
     IdRol int NOT NULL 
         CONSTRAINT FK_Employee_IdRol_Rol_IdRol
         FOREIGN KEY REFERENCES [Security].Rol (IdRol),
+    IdSalary int NOT NULL 
+        CONSTRAINT FK_Employee_IdSalary_Salary_IdSalary
+        FOREIGN KEY REFERENCES [Person].Salary (IdSalary),
     InssNumber VARCHAR(12) NOT NULL
         CONSTRAINT UQ_Employee_InssNumber
         UNIQUE NONCLUSTERED,
@@ -71,7 +84,7 @@ CREATE TABLE [Person].Employee(
     AuditDeleteDate datetime2(7) NULL
 );
 GO
-CREATE TABLE [Person].[User](
+CREATE TABLE [Account].[User](
      IdUser int IDENTITY(1,1) NOT NULL
         CONSTRAINT PK_User_IdUser
         PRIMARY KEY CLUSTERED,
@@ -88,6 +101,14 @@ CREATE TABLE [Person].[User](
     Avatar varchar (200) NOT NULL
         CONSTRAINT DF_User_Avatar_Image
             DEFAULT 'https://firebasestorage.googleapis.com/v0/b/lrs-tech-storage.appspot.com/o/default_avatar.png?alt=media&token=0710ac2a-3633-4a39-bc9e-151d0ecf26b3',
+    AuditCreateUser int NOT NULL,
+    AuditCreateDate datetime2(7) NOT NULL
+        CONSTRAINT DF_User_DefaultDate
+            DEFAULT GETDATE(),
+    AuditUpdateUser int NULL,
+    AuditUpdateDate datetime2(7) NULL,
+    AuditDeleteUser int NULL,
+    AuditDeleteDate datetime2(7) NULL
 );
 GO
 CREATE TABLE [Account].Request(
@@ -96,11 +117,27 @@ CREATE TABLE [Account].Request(
         PRIMARY KEY CLUSTERED,
     IdUser int NOT NULL
         CONSTRAINT FK_Request_IdUser_User_IdUser
-            FOREIGN KEY REFERENCES [Person].[User] (IdUser),
-    DateRequest date NOT NULL,
+            FOREIGN KEY REFERENCES [Account].[User] (IdUser),
+    DateRequest datetime DEFAULT GETDATE() NOT NULL,
+    Reason varchar(150) NOT NULL,
+    HouseDeed varchar(150) NOT NULL,
     StateRequest int NOT NULL
-    CONSTRAINT DF_Request_IsActive
-        DEFAULT 1,
+    CONSTRAINT DF_Request_IsPending
+        DEFAULT 0,
+);
+GO
+CREATE TABLE [Account].[Date](
+    IdDate int IDENTITY(1,1) NOT NULL
+        CONSTRAINT PK_AdoptionDate_IdAdoption
+        PRIMARY KEY CLUSTERED,
+    IdUser int NOT NULL
+        CONSTRAINT FK_Date_IdUser_User_IdUser
+            FOREIGN KEY REFERENCES [Account].[User] (IdUser),
+    [State] int NOT NULL
+        CONSTRAINT DF_Date_IsTrue
+            DEFAULT 1,
+    [Date] datetime not NULL,
+
 );
 GO
 CREATE TABLE [Security].Connection (
@@ -119,16 +156,6 @@ CREATE TABLE [Security].Connection (
     CreatedAt datetime2(7) DEFAULT GETDATE()
 );
 GO
-CREATE TABLE [Person].Salary(
-    IdSalary int IDENTITY(1,1) NOT NULL
-        CONSTRAINT PK_Salary_IdSalary
-            PRIMARY KEY CLUSTERED,
-    Idrol int NOT NULL
-        CONSTRAINT FK_Salary_IdRol_Rol_IdRol
-        FOREIGN KEY REFERENCES [Security].Rol (IdRol),
-    Quantity int NOT NULL,
-);
-GO
 CREATE TABLE[Pet].Breeds(
     IdBreed int IDENTITY(1,1) NOT NULL
         CONSTRAINT PK_Breeds_IdBreed
@@ -139,6 +166,15 @@ CREATE TABLE[Pet].Breeds(
     [Status] int NOT NULL
     CONSTRAINT DF_Breeds_IsActive
         DEFAULT 1,
+    InfoBreeds VARCHAR(150) NULL,
+    AuditCreateUser int NOT NULL,
+    AuditCreateDate datetime2(7) NOT NULL
+        CONSTRAINT DF_Breeds_DefaultDate
+            DEFAULT GETDATE(),
+    AuditUpdateUser int NULL,
+    AuditUpdateDate datetime2(7) NULL,
+    AuditDeleteUser int NULL,
+    AuditDeleteDate datetime2(7) NULL
 );
 GO
 CREATE TABLE[Pet].Dogs(
@@ -148,9 +184,18 @@ CREATE TABLE[Pet].Dogs(
     IdBreed int NOT NULL
         CONSTRAINT FK_Dogs_IdBreed_Breeds_IdBreed
         FOREIGN KEY REFERENCES [Pet].Breeds (IdBreed),
-    Birthdate date NOT NULL,
+    Birthdate date NULL,
     Color varchar(15) NULL,
+    Gender bit NOT NULL,
     Sickness int NOT NULL
     CONSTRAINT DF_Dogs_IsActive
         DEFAULT 1,
+    AuditCreateUser int NOT NULL,
+    AuditCreateDate datetime2(7) NOT NULL
+        CONSTRAINT DF_Dogs_DefaultDate
+            DEFAULT GETDATE(),
+    AuditUpdateUser int NULL,
+    AuditUpdateDate datetime2(7) NULL,
+    AuditDeleteUser int NULL,
+    AuditDeleteDate datetime2(7) NULL
 );
---
+++ TableScripts/Populate.sql
@@ -0,0 +1,96 @@
+USE PETS_DB
+GO
+INSERT INTO [Security].[Rol] (
+    [Name], 
+    RolInfo) 
+VALUES
+    ('Admin', 'Rol que se encarga de administrar el sistema.'),
+    ('Secretary', 'Administra las citas y solicitudes.'),
+    ('Client', 'Usuario que interactúa con el sistema.');
+GO
+INSERT INTO [Person].[Salary] (
+    [Quantity])
+VALUES
+    (6000),
+    (7000),
+    (8000);
+GO
+INSERT INTO [Person].[Person] (
+    [Names],
+    Surnames,
+    Telephone,
+    City,
+    Addres,
+    Birthdate,
+    AuditCreateUser
+    )
+VALUES
+    ('Samuel Elías', 'Solórzano Obando', 78907654, 'Managua', 'El arbolito 2 al lago', '2002-10-12',1),
+    ('Abel  Antonio', 'Calero Flores', 78907653, 'Managua', 'El arbolito 2 al lago', '2002-10-12', 1),
+    ('Ricardo Antonio', 'Rodriguez Borge', 78907652, 'Managua', 'El arbolito 2 al lago', '2002-10-12', 1)
+GO
+INSERT INTO [Person].[Employee] (
+  IdRol,
+  IdSalary,
+  InssNumber,
+  AuditCreateUser
+    )
+VALUES
+    (1, 3, 123456789012,1),
+    (2, 1, 123456789013,1)
+GO
+INSERT INTO [Account].[User] (
+    IdPerson,
+    IdRol,
+    Email,
+    [Password],
+    AuditCreateUser
+    )
+VALUES
+    (1, 1, 'samuelsol@gmail.com', 'samuel123',1),
+    (3, 3, 'ricardorod@gmail.com', 'ricardo123',1)
+GO
+INSERT INTO [Account].Request (
+    IdUser,
+    Reason,
+    HouseDeed
+    )
+VALUES
+    (1, 'Deseo adoptar un compañero de vida', '2023-01-25')
+GO
+INSERT INTO [Account].[Date] (
+    IdUser,
+    [Date]
+    )
+VALUES
+    (2, GETDATE())
+GO
+INSERT INTO [Pet].Breeds (
+    [Name],
+    InfoBreeds,
+    AuditCreateUser
+    )
+VALUES
+    ('Pastor alemán', 'Se trata de un perro bien proporcionado. La cabeza es ancha y se estrecha con gracia en un hocico afilado.',1)
+GO
+INSERT INTO [Pet].Dogs (
+    IdBreed,
+    Birthdate,
+    Color,
+    Gender,
+    AuditCreateUser
+    )
+VALUES
+    (1, '2021-10-20', 'Cafe castaño', 1,1)
+
+GO
+SELECT * FROM [Security].[Rol];
+SELECT * FROM  [Account].[Date];
+SELECT * FROM [Account].Request;
+SELECT * FROM [Pet].Dogs;
+SELECT * FROM [Pet].Breeds;
+SELECT * FROM [Security].Connection;
+SELECT * FROM[Account].[User];
+SELECT * FROM [Person].Employee;
+SELECT * FROM [Person].Salary;
+SELECT * FROM [Person].Person;
