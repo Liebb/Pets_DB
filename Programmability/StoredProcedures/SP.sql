cdde0ce680a2a273700f1e9f075cddfcdfdf615b
Liebb <liebermorazan@gmail.com>
2023-01-20 16:13:56 -0600

    Se corrigen los errores en los SP y se usan los esquemas

--- Programmability/StoredProcedures/SP.sql
+++ Programmability/StoredProcedures/SP.sql
@@ -1,72 +1,101 @@
 USE
 PETS_DB
 GO
+
 --Simulando un get de perros
-CREATE PROCEDURE Get_Dogs AS
+CREATE OR ALTER PROCEDURE [Pets].Get_Dogs AS
+BEGIN
     SELECT IdDogs, DogName, Color, Birthdate, Gender,BreedName , InfoBreeds 
         FROM [Pet].Dogs JOIN [Pet].[Breeds] 
             ON [Pet].Dogs.IdBreed =  [Pet].[Breeds].IdBreed
-    GO
-EXEC Get_Dogs
+    
+END
+EXEC [Pets].Get_Dogs
 GO 
+
 --POST DE DOGS
-CREATE PROCEDURE Post_Dogs 
+CREATE OR ALTER PROCEDURE [Pets].Post_Dogs 
     @IdBreed int, @DogName varchar(20), @Birthdate date,
     @Color varchar(15), @Gender bit, @Sickness int, @AuditCreateUser int 
-        AS 
-INSERT INTO [Pet].Dogs (
-    IdBreed,
-    DogName,
-    Birthdate,
-    Color,
-    Gender,
-    Sickness,
-    AuditCreateUser
+    AS
+BEGIN     
+    INSERT INTO [Pet].Dogs (
+        IdBreed,
+        DogName,
+        Birthdate,
+        Color,
+        Gender,
+        Sickness,
+        AuditCreateUser
     )
-VALUES
-    ( @IdBreed,@DogName, @Birthdate, @Color, @Gender, @Sickness, @AuditCreateUser);
-EXEC Post_Dogs @IdBreed = 1 ,@DogName = 'Vayne', @Birthdate = '2022-10-04', @Color = 'Marrón', @Gender = 1, @Sickness = 0, @AuditCreateUser =1
+    VALUES
+        ( @IdBreed,@DogName, @Birthdate, @Color, @Gender, @Sickness, @AuditCreateUser);
+END
+
+EXEC [Pets].Post_Dogs @IdBreed = 1 ,@DogName = 'Vayne', @Birthdate = '2022-10-04', @Color = 'Marrón', @Gender = 1, @Sickness = 0, @AuditCreateUser =1
 GO
+
+
 --GET DE USERS
-CREATE PROCEDURE Get_Users AS
-    SELECT  Names, Surnames, Telephone, City, Addres, Birthdate, Email, [Password], Avatar, [Name]
+CREATE OR ALTER PROCEDURE [Account].Get_Users AS
+BEGIN
+    SELECT  Names, Surnames, Telephone, City, Addres, Birthdate, Email, [Password], Avatar, [RolName]
         FROM [Account].[User] 
             JOIN [Person].Person ON [Account].[User].IdPerson = [Person].Person.IdPerson
             JOIN [Security].[Rol] ON [Account].[User] .IdRol = [Security].[Rol].[IdRol]
+END
 GO
-EXEC Get_Users
-GO
---Post de persona (OJO FALTA EL CAMPO DE AUDITORÍA)
-CREATE PROCEDURE Post_Person
-    @Names varchar(50), @Surnames varchar(50), @Telephone char(9), @City varchar(30),
-    @Addres varchar(150), @Birthdate date
-        AS 
-INSERT INTO [Person].Person (
-    Names,
-    Surnames,
-    Telephone,
-    City,
-    Addres,
-    Birthdate
-    )
-VALUES (@Names, @Surnames, @Telephone , @City, @Addres, @Birthdate);
-GO
---Post de Empleado
-CREATE PROCEDURE Post_Employee
-    @IdRol int, @IdSalary int, @InssNumber varchar(12)
-        AS 
-INSERT INTO [Person].Employee (
-    IdRol, 
-    IdSalary,
-    InssNumber
-    )
-VALUES (@IdRol, @IdSalary, @InssNumber);
 
+EXEC [Account].Get_Users
 GO
+
+
+--Post de persona (OJO FALTA EL CAMPO DE AUDITORÍA)
+CREATE OR ALTER PROCEDURE [Person].Post_Person
+    @Names varchar(50), @Surnames varchar(50), @Telephone char(9), @City varchar(30),
+    @Addres varchar(150), @Birthdate date,@AuditCreateUser int
+AS
+BEGIN 
+    DECLARE @Id int;    
+    INSERT INTO [Person].Person (
+        Names,
+        Surnames,
+        Telephone,
+        City,
+        Addres,
+        Birthdate,
+        AuditCreateUser
+        )
+    VALUES (@Names, @Surnames, @Telephone , @City, @Addres, @Birthdate, @AuditCreateUser); 
+    SET  @Id = SCOPE_IDENTITY();
+    SELECT IdPerson FROM [Person].Person WHERE IdPerson = @Id;
+END
+GO
+EXEC [Person].Post_Person 
+    @Names = 'Juan Kevin', @Surnames = 'Lopez Molina' ,@Telephone = '76239087', @City = 'Managua',
+    @Addres = 'Managua- Nicaragua', @Birthdate = '2022-10-20', @AuditCreateUser = 1;   
+GO
+
+
 --Post de Empleado
-CREATE PROCEDURE Post_User
+CREATE OR ALTER PROCEDURE [Person].Post_Employee
+    @IdRol int, @IdSalary int, @InssNumber varchar(12)
+        AS
+BEGIN 
+    INSERT INTO [Person].Employee (
+        IdRol, 
+        IdSalary,
+        InssNumber
+        )
+    VALUES (@IdRol, @IdSalary, @InssNumber);
+END
+GO
+
+--Post de Empleado
+CREATE OR ALTER PROCEDURE [Account].Post_User
     @IdPerson int, @IdRol int, @Email varchar(30), @Password varchar(30)
-        AS 
+        AS
+BEGIN 
 INSERT INTO [Person].Employee (
     IdPerson,
     IdRol, 
@@ -74,3 +103,13 @@ INSERT INTO [Person].Employee (
     InssNumber
     )
 VALUES (@IdPerson, @IdRol, @Email, @Password);
+END
+GO
+--Simulando un get de perros
+CREATE OR ALTER PROCEDURE [Security].Get_Rols AS
+BEGIN
+    SELECT *
+        FROM [Security].Rol  WHERE [Status] = 1
+END
+GO
+EXEC [Security].Get_Rols
